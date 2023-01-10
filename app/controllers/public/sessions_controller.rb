20c5dd369781422910b9e0fe4c414a492cdea8f6
ItohMarina <117239917+itohmarina@users.noreply.github.com>
2023-01-10 07:33:47 +0000

    installed Divise and add User, Admin tables, controllers

---
+++ app/controllers/public/sessions_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Public::SessionsController < Devise::SessionsController
+  before_action :user_state, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+  
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    if @user.valid_password?(params[:user][:password]) && @user.is_deleted==true
+      redirect_to new_user_session_path
+    end 
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #顧客用
+  devise_for :users,skip:[:passwords], controllers:{
+    registrations:"public/registrations",
+    sessions:'public/sessions'
+  }
+  #管理者用
+  devise_for :admins,skip:[:registrations, :passwords] ,controllers:{
+    registrations:"admin/registrations",
+    sessions:'admin/sessions'
+  }
+
+
+  root to:"homes#top"
+
 end
