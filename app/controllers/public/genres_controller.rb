b73a546fc33ea0ee239656f1bc548483cda1a5bf
ItohMarina <117239917+itohmarina@users.noreply.github.com>
2023-01-25 14:11:56 +0000

    Add public/genres controller

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -1,8 +1,9 @@
 class Public::GenresController < ApplicationController
-  
+
   def show
     @genre = Genre.find(params[:id])
-    @stories = @genre.stories.where(is_deleted: false, is_private: false)
-  end 
-  
+    stories = @genre.stories.where(is_deleted: false, is_private: false)
+    @stories = stories.select('stories.*', 'count(favorites.id) AS favs').left_joins(:favorites).group('stories.id').order('favs desc')
+  end
+
 end
