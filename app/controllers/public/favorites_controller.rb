23f296a3b174a2347a7888a36300eb1606c6afbf
ItohMarina <117239917+itohmarina@users.noreply.github.com>
2023-01-27 12:14:39 +0000

    favorite#show

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,21 +3,19 @@ class Public::FavoritesController < ApplicationController
   
   def show
     @user=User.find(params[:id])
-    @stories=@user.favorite_stories
+    @stories=@user.favorite_stories.page(params[:page])
   end
 
   def create
     @story=Story.find(params[:format])
     favorite=current_user.favorites.new(story_id: @story.id)
     favorite.save
-    #redirect_to request.referer
   end
 
   def destroy
     @story=Story.find(params[:id])
     favorite=current_user.favorites.find_by(story_id: @story.id)
     favorite.destroy
-    #redirect_to request.referer
   end
 
 
